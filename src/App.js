7ec24cc17b4ed3696b3ecb95178b50f65dacd84f
kat8335 <carreno.katerina@gmail.com>
2022-12-10 02:57:22 +0100

    added the GitHub repository

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <Weather />
-    </div>
+    <>
+     <div className="weather-app-wrapper">
+      <div className="App">
+        <Weather />
+      </div>
+      <small>
+        <a
+          href="https://github.com/KatzerinaC/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Katerina Carreno{" "}
+      </small>
+        </div>
+    </>
   );
 }
 
